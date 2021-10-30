42a64fe6faed996f2e98086d303e349b6d7e9fc1
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2021-10-30 16:22:44 -0500

    test postgres database

--- routes.go
+++ routes.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -14,6 +15,21 @@ func (a *application) routes() *chi.Mux {
 	a.App.Routes.Get("/go-page", a.Handlers.GoPage)
 	a.App.Routes.Get("/jet-page", a.Handlers.JetPage)
 	a.App.Routes.Get("/sessions", a.Handlers.SessionTest)
+
+	a.App.Routes.Get("/test-database", func(rw http.ResponseWriter, r *http.Request) {
+		query := "select id, first_name from users where id = 1"
+		row := a.App.DB.Pool.QueryRowContext(r.Context(), query)
+
+		var id int
+		var name string
+		err := row.Scan(&id, &name)
+		if err != nil {
+			a.App.ErrorLog.Println(err)
+			return
+		}
+
+		fmt.Fprintf(rw, "%d %s", id, name)
+	})
 
 	// static assets
 	fileServer := http.FileServer(http.Dir("./public"))
