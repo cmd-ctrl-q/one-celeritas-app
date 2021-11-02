645dfc6b756c4802fbf98fa063910f39cafed02f
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2021-11-01 23:30:15 -0500

    set up integration testing with docker

---
+++ data/integration_test.go
@@ -0,0 +1,88 @@
+package data
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/ory/dockertest/v3"
+	"github.com/ory/dockertest/v3/docker"
+)
+
+var (
+	host     = "localhost"
+	user     = "postgres"
+	password = "secret"
+	dbName   = "celeritas_test"
+	// port = "5432"
+	port = "5435"
+	dsn  = "host=%s port=%s user=%s password=%s dbname=%s sslmode=disable timezone=UTC connect_timeout=5"
+)
+
+var dummyUser = User{
+	FirstName: "Some",
+	LastName:  "Guy",
+	Email:     "me@here.com",
+	Active:    1,
+	Password:  "password",
+}
+
+var models Models
+var testDB *sql.DB
+var resource *dockertest.Resource
+var pool *dockertest.Pool
+
+func TestMain(m *testing.M) {
+	os.Setenv("DATABASE_TYPE", "postgres")
+
+	// get docker image and run it
+	p, err := dockertest.NewPool("")
+	if err != nil {
+		log.Fatalf("could not connect to docker: %s", err)
+	}
+
+	pool = p
+
+	opts := dockertest.RunOptions{
+		Repository: "postgres",
+		Tag:        "13.4", // postgres version
+		Env: []string{
+			"POSTGRES_USER=" + user,
+			"POSTGRES_PASSWORD=" + password,
+			"POSTGRES_DB=" + dbName,
+		},
+		// ports to expose in docker image
+		ExposedPorts: []string{"5432"},
+		// bind ports to local ports
+		PortBindings: map[docker.Port][]docker.PortBinding{
+			"5432": {
+				{HostIP: "0.0.0.0", HostPort: port},
+			},
+		},
+	}
+
+	// run resource
+	resource, err = pool.RunWithOptions(&opts)
+	if err != nil {
+		_ = pool.Purge(resource)
+		log.Fatalf("could not start resource: %s", err)
+	}
+
+	// wait for docker to build the db.
+	// ie continue to retry until ping is successful
+	if err = pool.Retry(func() error {
+		var err error
+		testDB, err = sql.Open("pgx", fmt.Sprintf(dsn, host, port, user, password, dbName))
+		if err != nil {
+			return err
+		}
+		return testDB.Ping()
+	}); err != nil {
+		_ = pool.Purge(resource)
+		log.Fatalf("could not connect to docker: %s", err)
+	}
+
+	// populate db with tables
+}
