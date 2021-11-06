2a2df500b46a7fe01bb62fe0742744cdda0189a2
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2021-11-06 14:21:34 -0500

    add get form route for retreiving a form and populating it with data; create form.jet

---
+++ handlers/form-val-handlers.go
@@ -0,0 +1,23 @@
+package handlers
+
+import (
+	"myapp/data"
+	"net/http"
+
+	"github.com/CloudyKit/jet/v6"
+)
+
+// display the form
+func (h *Handlers) Form(w http.ResponseWriter, r *http.Request) {
+	vars := make(jet.VarMap)
+	validator := h.App.Validator(nil)
+	vars.Set("validator", validator)
+	vars.Set("user", data.User{})
+
+	err := h.App.Render.Page(w, r, "form", vars, nil)
+	if err != nil {
+		h.App.ErrorLog.Println(err)
+	}
+}
+
+// submit form and validate data
