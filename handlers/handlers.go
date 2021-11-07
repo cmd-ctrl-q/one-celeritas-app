f49bb41f13d5a58fe2b84b30c2300e00896cea2f
cmd-ctrl-q <cmd.ctrl.q@gmail.com>
2021-11-07 00:56:34 -0500

    add encryption/decryption

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"fmt"
 	"myapp/data"
 	"net/http"
 
@@ -89,4 +90,29 @@ func (h *Handlers) XML(w http.ResponseWriter, r *http.Request) {
 
 func (h *Handlers) DownloadFile(w http.ResponseWriter, r *http.Request) {
 	h.App.DownloadFile(w, r, "./public/images", "celeritas.jpg")
+}
+
+func (h *Handlers) TestCrypto(w http.ResponseWriter, r *http.Request) {
+	plainText := "Hello, world"
+	fmt.Fprint(w, "Unencrypted: "+plainText+"\n")
+
+	// encrypt
+	encrypted, err := h.encrypt(plainText)
+	if err != nil {
+		h.App.ErrorLog.Println(err)
+		h.App.Error500(w, r)
+		return
+	}
+
+	fmt.Fprint(w, "Encrypted: "+encrypted+"\n")
+
+	// decrypt
+	decrypted, err := h.decrypt(encrypted)
+	if err != nil {
+		h.App.ErrorLog.Println(err)
+		h.App.Error500(w, r)
+		return
+	}
+	fmt.Fprint(w, "Decrypted: "+decrypted+"\n")
+
 }
